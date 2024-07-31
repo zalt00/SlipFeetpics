d126bb9c37777ed9701e5eb1e195a56198844e4a
Zalt0 <66617933+Zalt0@users.noreply.github.com>
2024-07-31 15:02:14 +0200

    previsu2

--- scripts/antimatter_player.gd
+++ scripts/antimatter_player.gd
@@ -5,6 +5,8 @@ extends Node3D
 var breakable: CSGCombiner3D
 @export var projectile: Node3D
 
+@onready var previsu_meshi := $Previsu
+
 var copies = []
 
 func _ready():
@@ -29,3 +31,7 @@ func _process(delta):
 			obj.queue_free()
 			
 		print(len(copies))
+	if Input.is_action_pressed("viser"):
+		previsu_meshi.show()
+	else:
+		previsu_meshi.hide()
