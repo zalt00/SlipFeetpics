05ef0ba134ee34f8bede61727227efc6df66ed6c
Skyxo <charlesb100000@gmail.com>
2024-08-01 13:49:33 +0200

    light_charles

--- scripts/switch_charles.gd
+++ scripts/switch_charles.gd
@@ -1,15 +1,28 @@
 extends StaticBody3D
 
 @export var lights : Array[Node3D]
+@export var pressed = false
 
 var pressing = false
-var pressed = false
 
 @onready var mat_off = load("res://materials/wire_off.tres")
 @onready var mat_on = load("res://materials/wire_on.tres")
 @onready var lever = $Lever
 
 var animation_speed = 3
+
+func _ready():
+	for light in lights:
+		var bulb = light.get_node("bulb")
+		var spot = light.get_node("spot")
+		if spot:
+			spot.visible = pressed
+		if bulb:
+			if pressed:
+				bulb.material_override = mat_on
+			else:
+				bulb.material_override = mat_off
+	pressing = !pressing
 
 func interact():
 	for light in lights:
@@ -18,23 +31,32 @@ func interact():
 		if spot:
 			spot.visible = !spot.visible
 		if bulb:
-			if spot.visible:
-				bulb.set_material(mat_on)
+			if pressed:
+				bulb.material_override = mat_on
 			else:
-				bulb.set_material(mat_off)
+				bulb.material_override = mat_off
 	pressing = !pressing
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if pressing and not pressed:
-		if lever.rotation_degrees.x < 135:
-			lever.rotate_x(animation_speed * delta)
+	
+	if pressing:
+		if not pressed:
+			if lever.rotation_degrees.x < 135:
+				lever.rotate_x(animation_speed * delta)
+			else:
+				pressed = true
+				lever.rotation_degrees.x = 135
+				pressing = false
 		else:
-			pressed = true
+			if lever.rotation_degrees.x > 45:
+				lever.rotate_x(-animation_speed * delta)
+			else:
+				pressed = false
+				lever.rotation_degrees.x = 45
+				pressing = false
+		
+	else:
+		if pressed:
 			lever.rotation_degrees.x = 135
-	elif not pressing and lever.rotation_degrees.x > 0:
-		lever.rotate_x(-animation_speed * delta)
-		if lever.rotation_degrees.x <= 45:
+		else:
 			lever.rotation_degrees.x = 45
-			pressed = false
-
