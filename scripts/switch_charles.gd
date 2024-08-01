bf494ae6fc8204adfcfc10df17c8354528d453f5
Skyxo <charlesb100000@gmail.com>
2024-08-01 13:33:54 +0200

    commit_charles

--- scripts/switch_charles.gd
+++ scripts/switch_charles.gd
@@ -5,14 +5,23 @@ extends StaticBody3D
 var pressing = false
 var pressed = false
 
+@onready var mat_off = load("res://materials/wire_off.tres")
+@onready var mat_on = load("res://materials/wire_on.tres")
 @onready var lever = $Lever
 
 var animation_speed = 3
 
 func interact():
 	for light in lights:
-		if light:
-			light.visible = !light.visible
+		var bulb = light.get_node("bulb")
+		var spot = light.get_node("spot")
+		if spot:
+			spot.visible = !spot.visible
+		if bulb:
+			if spot.visible:
+				bulb.set_material(mat_on)
+			else:
+				bulb.set_material(mat_off)
 	pressing = !pressing
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
