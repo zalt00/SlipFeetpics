fa2032efa164abb1b4ae73d174c6bde2e68e2e3e
Zalt0 <66617933+Zalt0@users.noreply.github.com>
2024-08-02 15:20:01 +0200

    trucs

---
+++ scripts/AnimatableBody3D.gd
@@ -0,0 +1,19 @@
+extends AnimatableBody3D
+
+@export var player: CharacterBody3D
+
+var move = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	if move:
+		position.x += delta*10.0
+
+
+func _on_area_3d_body_entered(body):
+	if body == player:
+		move = true
