3af64731d237ca32c7f510b43f240571edc0cba8
Zalt0 <66617933+Zalt0@users.noreply.github.com>
2024-08-02 18:32:55 +0200

    yiighf

--- scripts/AnimatableBody3D.gd
+++ scripts/AnimatableBody3D.gd
@@ -10,8 +10,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	if move:
-		position.x += delta*10.0
+	if move and position.z > -30:
+		position.z += -delta*10.0
+		
 
 
 func _on_area_3d_body_entered(body):
