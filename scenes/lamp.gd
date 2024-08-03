e3aa65cfb0090c8c9307201e16f312c2debd41da
lechefotacos <professionalgragas@gmail.com>
2024-08-03 16:34:56 +0200

    llllllllplk^hpiuhuihlihihpoiu,pou,ioiji

--- scenes/lamp.gd
+++ scenes/lamp.gd
@@ -3,12 +3,12 @@ extends Node3D
 
 @onready var mat_off = load("res://materials/wire_off.tres")
 @onready var mat_on = load("res://materials/wire_on.tres")
+@export var energy : float = 10.0
 
 @export var already_on = false
 var on = false :
 	set(value):
 		$base/bulb/spot.visible = value
-		$base/bulb/spot2.visible = value
 		
 		if value:
 			$base/bulb.set_surface_override_material(0, mat_on)
@@ -23,4 +23,6 @@ func _ready():
 	mat_on.emission = Color(1.0, 0.95, 0.0, 1.0)
 	
 	on = already_on
+	
+	$base/bulb/spot.light_energy = energy
 
