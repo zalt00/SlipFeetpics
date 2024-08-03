08c602e69062ded4f51ac69cff5a252d1d2e3bb0
Docteur-Raton <docteur.raton@gmail.com>
2024-08-04 01:05:35 +0200

    Implémente les sliders de volume

--- scripts/volume_slider.gd
+++ scripts/volume_slider.gd
@@ -5,9 +5,9 @@ extends HSlider
 var bus_index: int
 
 func _ready() -> void:
-	value = AudioServer.get_bus_volume_db(bus_index)
-	var bus_index = AudioServer.get_bus_index(bus_name)
+	bus_index = AudioServer.get_bus_index(bus_name)
 	value_changed.connect(_on_value_changed)
+	value = db_to_linear(AudioServer.get_bus_volume_db(bus_index))
 
 func _on_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
